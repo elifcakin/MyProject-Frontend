8cda9f4a3e560144a62622c827b3240f66281b74
Atk <atakanarslan@msn.com>
2022-09-20 17:48:48 +0300

    20.09.22
    20.09.22

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -25,7 +25,7 @@ class UserSignupPage extends React.Component{
             displayName:this.state.displayName,
             password:this.state.password
         }
-        axios.post('http://localhost:8080/api/1.0/users', body)
+        axios.post('/api/1.0/users', body)
     }
     render() {
         return(
