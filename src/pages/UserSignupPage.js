7263a30649cd2e189b2ad73179669213bafed73e
Atk <atakanarslan@msn.com>
2022-09-22 17:46:14 +0300

    22.09.22
    17.46

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import axios from 'axios';
+import { ThemeProvider } from 'react-bootstrap';
+import { signup } from '../api/apiCalls';
 class UserSignupPage extends React.Component{
 
     state = {
@@ -7,17 +8,19 @@ class UserSignupPage extends React.Component{
         displayName: null,
         password: null,
         passwordRepeat: null,
-        pendingApiCall:false
-
-
+        pendingApiCall:false,
+        errors: {}
     };
 
     onChange = event => {
        const { name, value } = event.target;
+       const errors = {...this.state.errors}
+       errors[name] = undefined
         this.setState({
-            [name]:value
-        })
-    }
+            [name]:value,
+            errors 
+        });
+    };
     onClickSignup = event => {
         event.preventDefault();
 
@@ -31,21 +34,44 @@ class UserSignupPage extends React.Component{
         this.setState({ pendingApiCall: true});
 
 
-        axios.post('/api/1.0/users', body)
+        try {
+           const response = signup(body);
+        }  catch  (error) {
+           if (error.response.data.validationErrors) {
+             this.setState({ errors: error.response.data.validationErrors});
+           }
+          
+        }
+
+        this.setState({pendingApiCall: false});
+
+        //signup(body)
+        //  .then((response) => {
+        //    this.setState({ pendingApiCall: false});  
+        //  }).catch(error => {S
+        //    this.setState({ pendingApiCall: false});
+        //  });
+        
+        
     };
     
     render() {
+        const { pendingApiCall, errors  } = this.state; 
+        const { username , displayName } = errors;
+
         return(
             <div className = "container"> 
               <form>
                <h1 className= " text-center">Sign Up</h1> 
                <div className = "form-group">
                  <label>Username</label>
-                 <input className="form-control" name="username" onChange={this.onChange} />
+                 <input className={username ? 'form-control is-invalid ': 'form-control '} name="username" onChange={this.onChange} />
+                 <div className="invalid-feedback">{username} </div>
                </div>
                <div className = "form-group">
                  <label>Display Name</label>
-                 <input className="form-control" name="displayName" onChange={this.onChange}/> 
+                 <input className={displayName ? 'form-control is-invalid ': 'form-control '} name="displayName" onChange={this.onChange}/> 
+                 <div className="invalid-feedback">{displayName} </div>
                </div>
                <div className = "form-group">
                  <label>Password </label>
@@ -56,9 +82,8 @@ class UserSignupPage extends React.Component{
                  <input className="form-control" name="passwordRepeat" type= "password" onChange={this.onChange}/>
                </div>
                <div className="text-center">
-                <button 
-                  className="btn btn-primary" onClick= {this.onClickSignup} disabled={this.state.pendingApiCall} >
-                    Sign Up
+                <button className="btn btn-primary" onClick= {this.onClickSignup} disabled={pendingApiCall} >
+                    {pendingApiCall && <span className="spinner-border spinner-border-sm" ></span> } Sign Up
                 </button>
                </div>
               </form>
